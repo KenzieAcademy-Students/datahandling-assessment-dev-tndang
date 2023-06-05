630e8a4b5a5928969a54bb00d25c2b99a2ff6826
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-06-05 10:19:16 -0400

    Completed Part 1 by first creating a new array matching and filtering the match titles from the movie details API and added to a new array contianing the needed info with the movies info.

--- code.js
+++ code.js
@@ -1,4 +1,9 @@
 // Your Code Here!
+let movieInfo = movies.map(movie => movieDetails.filter(details => details.title === movie.title));
+let movieCatalog = [...movies, ...movieInfo];
+    
+
+document.write(JSON.stringify(movieCatalog));
 
 console.log("Movies:", movies);
 console.log("MovieDetails:", movieDetails);
