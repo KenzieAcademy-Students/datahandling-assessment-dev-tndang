9ccf71b63a111cfdf991479afcab379082aa55c5
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-06-06 22:48:57 -0400

    Got a basic search funtion active, now going to render the required completion criteria

--- code.js
+++ code.js
@@ -1,13 +1,24 @@
 // Your Code Here!
 let movieInfo = movies.map(movie => movieDetails.filter(details => details.title === movie.title));
-let movieCatalog = [...movies, ...movieInfo];
+let movieCatalog = [...movies, ...movieInfo].filter(movie => movie.title !== undefined);
 
 let movieListDiv = document.getElementById("movieList");
-let movieListRender = movieCatalog
-    .filter(movie => movie.title !== undefined)
-    .forEach(movie => {
+let movieListRender = movieCatalog.forEach(movie => {
     movieListDiv.innerHTML += `<li>${movie.title}</li>`
 })
+
+let searchButton = document.getElementById("searchButton");
+let searchBar = document.getElementById("searchBar");
+
+searchButton.addEventListener("click", () => {
+    movieListDiv.innerHTML = "";
+    movieListRender = movieCatalog.filter(function(movie) {
+        return movie.title.toLowerCase().includes(searchBar.value.toLowerCase());
+    });
+    movieListRender.forEach(result => {
+        movieListDiv.innerHTML += `<li>${result.title}</li>`
+    });
+});
 
 // document.write(JSON.stringify(movieCatalog));
 
