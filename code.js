a8490d32fe1a89bfd7f2b8062c9d3f4699400042
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-06-06 22:34:10 -0400

    Rendered the new array as a list to visually verify the list, and then filtered it to removed any undefined in the new list

--- code.js
+++ code.js
@@ -1,9 +1,15 @@
 // Your Code Here!
 let movieInfo = movies.map(movie => movieDetails.filter(details => details.title === movie.title));
 let movieCatalog = [...movies, ...movieInfo];
-    
 
-document.write(JSON.stringify(movieCatalog));
+let movieListDiv = document.getElementById("movieList");
+let movieListRender = movieCatalog
+    .filter(movie => movie.title !== undefined)
+    .forEach(movie => {
+    movieListDiv.innerHTML += `<li>${movie.title}</li>`
+})
+
+// document.write(JSON.stringify(movieCatalog));
 
 console.log("Movies:", movies);
 console.log("MovieDetails:", movieDetails);
