0ca6f017daa66af1a303383f8fc34e71b41780bf
Thien Dang <thien.dang@mykenzie.snhu.edu>
2023-06-06 23:37:59 -0400

    Got basic styling in, next up is finishing the css styling, and info

--- code.js
+++ code.js
@@ -3,8 +3,11 @@ let movieInfo = movies.map(movie => movieDetails.filter(details => details.title
 let movieCatalog = [...movies, ...movieInfo].filter(movie => movie.title !== undefined);
 
 let movieListDiv = document.getElementById("movieList");
+// let movieListDivID = "";
+
 let movieListRender = movieCatalog.forEach(movie => {
-    movieListDiv.innerHTML += `<li>${movie.title}</li>`
+    // movieListDivID = `${movie.title.toLowerCase()}`.replace(/[^a-z0-9]+/ig, "");
+    movieListDiv.innerHTML += `<div class="movieDiv">${movie.title}</div>`;
 })
 
 let searchButton = document.getElementById("searchButton");
